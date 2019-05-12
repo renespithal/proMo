1d4fe383fa3b29ff72090198673373a1923b2ad0
Rene Nespithal <rene.nespithal@gmail.com>
2019-05-12 15:33:04 +0200

    Update H2-3.hs

--- H2-3.hs
+++ H2-3.hs
@@ -1,7 +1,10 @@
+-- ich gehe davon aus das u != U
 count :: Char -> String -> Int 
 count n [] = 0
 count n (x:xs) = case n==x of
                     True  -> 1 + count n xs
                     False -> 0 + count n xs
 
---freq :: String -> [(Char, Int)]
+-- wusste nicht wie ich ein Set in Haskell mache deswegen einfach über A-z iteriert
+freq :: String -> [(Char, Int)]
+freq xs = [(x,n) | x <- ['A'..'z'], let n = (length.filter (==x)) xs, n > 0]
