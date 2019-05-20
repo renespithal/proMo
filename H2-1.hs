03af7224c0f93f1b4f0b68bcfa83a5d35591d426
Rene Nespithal <rene.nespithal@gmail.com>
2019-05-20 16:03:30 +0200

    Update H2-1.hs

--- H2-1.hs
+++ H2-1.hs
@@ -4,33 +4,25 @@ sort2 (x,y) = case x > y of
                 True -> (y,x)
                 _ -> (x,y)
 
-sort3 :: (Int , Int , Int) -> (Int , Int , Int)
-sort3 (x,y,z)  = case x<y && y<z of
-                   True -> (x,y,z)
-                   _ -> case x<z && y<z of
-                     True -> (y,x,z)
-                     _ -> case z<y && y<x of
-                       True -> (z,y,x)
-                       _ -> case y<z of
-                         True -> (y,z,x)
-                         _ -> case x<z of
-                           True -> (x,z,y)
-                           _ -> (z,x,y)
+sort3 :: (Int,Int,Int) -> (Int,Int,Int)
+sort3 (x,y,z)
+  | x<y && y<z = (x,y,z)
+  | x<z && y<z = (y,x,z)
+  | z<y && y<x = (z,y,x)
+  | y<z        = (y,z,x)
+  | x<z        = (x,z,y)
+  | otherwise  = (z,x,y)
 
 sortFirst3 :: [Int] -> [Int]
 sortFirst3 [] = []
 sortFirst3 [x] = [x]
-sortFirst3 [x,y] = case x > y of
-                      True -> [y,x]
-                      _ -> [x,y]
-sortFirst3 (x:y:z:xs) = case x<y && y<z of
-                           True -> [x,y,z] ++ xs
-                           _ -> case x<z && y<z of
-                             True -> [y,x,z] ++ xs
-                             _ -> case z<y && y<x of
-                               True -> [z,y,x] ++ xs
-                               _ -> case y<z of
-                                 True -> [y,z,x] ++ xs
-                                 _ -> case x<z of
-                                   True -> [x,z,y] ++ xs
-                                   _ -> [z,x,y] ++ xs
+sortFirst3 [x,y]
+  | x > y     = [y,x]
+  | otherwise = [x,y]
+sortFirst3 (x:y:z:xs)
+  | x<y && y<z = [x,y,z] ++ xs
+  | x<z && y<z = [y,x,z] ++ xs
+  | z<y && y<x = [z,y,x] ++ xs
+  | y<z        = [y,z,x] ++ xs
+  | x<z        = [x,z,y] ++ xs
+  | otherwise  = [z,x,y] ++ xs
