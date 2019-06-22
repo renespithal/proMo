2c6fa3cda4d74f489af517f94ef62a7074f5418a
Rene Nespithal <rene.nespithal@gmail.com>
2019-06-22 23:22:57 +0200

    Update H7-1.hs

--- H7-1.hs
+++ H7-1.hs
@@ -1,12 +1,9 @@
+import qualified Data.List as Gr
 binaryToIntList:: [Bool] -> Int
 binaryToIntList xs = sum (map (*2^2)(map fromEnum xs))
 
 upto :: Eq a => (a -> Bool) -> [a] -> [a]
 upto f xs = filter f xs
 
-removeAdjacentDuplicates :: (Ord a) => [a] -> [a]
-removeAdjacentDuplicates = map head . group . sort
--- [3,4,2,1,2,3,4,1].
--- sort -> [1,1,2,2,3,3,4,4]
--- group -> [[1, 1], [2, 2], [3,3], [4,4]];
--- head -> [1, 2, 3, 4]
+removeAdjacentDuplicates :: Eq a => [a] -> [a]  
+removeAdjacentDuplicates = map head . Gr.group
