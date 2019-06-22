48ea0248414899234764154c527b3019598af915
Rene Nespithal <rene.nespithal@gmail.com>
2019-06-23 00:05:37 +0200

    Update H7-1.hs

--- H7-1.hs
+++ H7-1.hs
@@ -1,9 +1,10 @@
 import qualified Data.List as Gr
 binaryToIntList:: [Bool] -> Int
-binaryToIntList xs = sum (map (*2^2)(map fromEnum xs))
+binaryToIntList [] = error("List cant be empty")
+binaryToIntList xs = sum(zipWith (\i x -> x * 2^i) [length xs -1, length xs -2..0] (map fromEnum xs))
 
 upto :: Eq a => (a -> Bool) -> [a] -> [a]
-upto f xs = filter f xs
+upto f xs = filter (not . f) xs
 
 removeAdjacentDuplicates :: Eq a => [a] -> [a]  
 removeAdjacentDuplicates = map head . Gr.group
